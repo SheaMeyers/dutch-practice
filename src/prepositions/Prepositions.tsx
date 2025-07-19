fabc73ce0a3ae44d6f89da6970ddd13bb7539614
Shea Meyers <shea.meyers@gmail.com>
2025-07-19 15:45:05 +0200

    Add questions and navigation

--- src/prepositions/Prepositions.tsx
+++ src/prepositions/Prepositions.tsx
@@ -1,18 +1,68 @@
-import './Prepositions.css'
+import { useState } from 'react';
+import "./Prepositions.css";
+
+interface Question {
+  word: string;
+  options: string[];
+  answer: string;
+}
+
+const questions: Question[] = [
+    {
+        word: 'aanbeiden',
+        options: ['op', 'aan', 'bij', 'met'],
+        answer: 'aan'
+    },
+    {
+        word: 'aandringen',
+        options: ['op', 'aan', 'bij', 'met'],
+        answer: 'op'
+    },
+    {
+        word: 'aankleden',
+        options: ['op', 'aan', 'bij', 'met'],
+        answer: 'met'
+    }
+]
 
 const Prepositions = () => {
+
+    const [questionNumber, setQuestionNumber] = useState<number>(0);
+
   return (
     <div className="Prepositions">
       <p>Choose the correct preposition for the verb</p>
-      <p className='h3 Verb'>Aanbeiden</p>
-      <div className='Choices'>
-        <button className='btn btn-light Choices__Button' type="button">Op</button>
-        <button className='btn btn-light Choices__Button' type="button">Aan</button>
-        <button className='btn btn-light Choices__Button' type="button">Bij</button>
-        <button className='btn btn-light Choices__Button' type="button">Met</button>
+      <p className="h3 Verb">{questions[questionNumber].word}</p>
+      <div className="Choices">
+        {questions[questionNumber].options.map(option => 
+            <button 
+                className="btn btn-light Choices__Button" 
+                type="button"
+            >
+                {option}
+            </button>
+        )}
+      </div>
+      <div className="Navigator">
+            <button
+                disabled={questionNumber === 0}
+                className="btn btn-secondary" 
+                type="button"
+                onClick={() => setQuestionNumber(questionNumber-1)}
+            >
+                Previous
+            </button>
+            <button
+                disabled={questionNumber === (questions.length-1)}
+                className="btn btn-primary" 
+                type="button"
+                onClick={() => setQuestionNumber(questionNumber+1)}
+            >
+                Next
+            </button>
       </div>
     </div>
   );
 };
 
-export default Prepositions;
+export default Prepositions
