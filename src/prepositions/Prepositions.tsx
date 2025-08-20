5d57313ce8126d922b08acb20d75ed2b89c6d4c0
SheaMeyers <shea.meyers@gmail.com>
2025-08-20 19:25:54 +0200

    Function to get options is working

--- src/prepositions/Prepositions.tsx
+++ src/prepositions/Prepositions.tsx
@@ -13,18 +13,21 @@ const options: string[] = [
     'aan', 'met', 'op', 'naar', 'voor']
 
 const getOptions = (correctAnswer: string): string[] => {
+
     const retrievedOptions: string[] = []
 
     // Get three random options
     while (retrievedOptions.length < 3) {
-        var optionIndex = Math.random() * (options.length - 2)
-        if (!retrievedOptions.includes(options[optionIndex])) {
-            retrievedOptions.push(options[optionIndex])
+        const randomIndex = Math.floor(Math.random() * options.length);
+        const randomOption = options[randomIndex]
+
+        if (randomOption !== correctAnswer && !retrievedOptions.includes(randomOption)) {
+            retrievedOptions.push(randomOption)
         }
     }
 
     // Put the correct answer at a random spot
-    var randomIndex = Math.random() * 4
+    const randomIndex = Math.random() * 4
     retrievedOptions.splice(randomIndex, 0, correctAnswer)
 
     return retrievedOptions
