1df2c852f1131111fe25372434a22405f99de531
SheaMeyers <shea.meyers@gmail.com>
2025-08-23 15:25:59 +0200

    Show other possible answers

--- src/prepositions/Prepositions.tsx
+++ src/prepositions/Prepositions.tsx
@@ -2,17 +2,18 @@ import { useReducer } from 'react';
 import "./Prepositions.css";
 
 interface Question {
-  word: string;
-  answer: string;
+    word: string;
+    answer: string;
+    otherAnswers: string[]
 }
 
 const options: string[] = [
-    'in', 'naast', 'langs', 'bij', 'rond', 
-    'tot', 'door', 'op.', 'onder', 'van', 
-    'tegen', 'uit', 'over', 'om', 'spuiten', 
+    'in', 'naast', 'langs', 'bij', 'rond',
+    'tot', 'door', 'op', 'onder', 'van',
+    'tegen', 'uit', 'over', 'om', 'spuiten',
     'aan', 'met', 'op', 'naar', 'voor']
 
-const getOptions = (correctAnswer: string): string[] => {
+const getOptions = (correctAnswer: string, otherAnswers: string[]): string[] => {
 
     const retrievedOptions: string[] = []
 
@@ -21,7 +22,9 @@ const getOptions = (correctAnswer: string): string[] => {
         const randomIndex = Math.floor(Math.random() * options.length);
         const randomOption = options[randomIndex]
 
-        if (randomOption !== correctAnswer && !retrievedOptions.includes(randomOption)) {
+        if (randomOption !== correctAnswer &&
+            !retrievedOptions.includes(randomOption) &&
+            !otherAnswers.includes(randomOption)) {
             retrievedOptions.push(randomOption)
         }
     }
@@ -36,41 +39,54 @@ const getOptions = (correctAnswer: string): string[] => {
 const questions: Question[] = [
     {
         word: 'aanbeiden',
-        answer: 'aan'
+        answer: 'aan',
+        otherAnswers: []
     },
     {
         word: 'aandringen',
-        answer: 'op'
+        answer: 'op',
+        otherAnswers: ['in', 'aan']
     },
     {
         word: 'aankleden',
-        answer: 'met'
+        answer: 'met',
+        otherAnswers: []
     }
 ]
 
 type PrepositionsState = {
-  options: string[]
-  questionNumber: number
-  answer: string
+    options: string[]
+    questionNumber: number
+    answer: string
 };
 
 type DispatcherActions =
-  | { type: 'previousQuestion' }
-  | { type: 'nextQuestion' }
-  | { type: 'giveAnswer', payload: string };
+    | { type: 'previousQuestion' }
+    | { type: 'nextQuestion' }
+    | { type: 'giveAnswer', payload: string };
 
 const initialState: PrepositionsState = {
-    options: getOptions(questions[0].answer),
+    options: getOptions(questions[0].answer, questions[0].otherAnswers),
     questionNumber: 0,
     answer: ''
 }
 
 const prepositionsReducer = (state: PrepositionsState, action: DispatcherActions): PrepositionsState => {
-    switch(action.type) {
+    switch (action.type) {
         case 'previousQuestion':
-            return { questionNumber: state.questionNumber-1, answer: '', options: getOptions(questions[state.questionNumber-1].answer) }
+            const previousQuestionNumber = state.questionNumber - 1
+            return {
+                questionNumber: previousQuestionNumber,
+                answer: '',
+                options: getOptions(questions[previousQuestionNumber].answer, questions[previousQuestionNumber].otherAnswers)
+            }
         case 'nextQuestion':
-            return { questionNumber: state.questionNumber+1, answer: '', options: getOptions(questions[state.questionNumber+1].answer) }
+            const nextQuestionNumber = state.questionNumber + 1
+            return {
+                questionNumber: nextQuestionNumber,
+                answer: '',
+                options: getOptions(questions[nextQuestionNumber].answer, questions[nextQuestionNumber].otherAnswers)
+            }
         case 'giveAnswer':
             return { questionNumber: state.questionNumber, answer: action.payload, options: state.options }
         default:
@@ -83,48 +99,56 @@ const Prepositions = () => {
     const [prepositionsState, dispatch] = useReducer(prepositionsReducer, initialState)
     const { options, questionNumber, answer } = prepositionsState
 
-  return (
-    <div className="Prepositions">
-      <p>Choose the correct preposition for the verb</p>
-      <p className="h3 Verb">{questions[questionNumber].word}</p>
-      <div className="Choices">
-        {options.map(option => 
-            <button 
-                className={`btn Choices__Button ${!answer && 'btn-light'} ${answer === option ? 'btn-lg' : 'btn-sm'} ${answer && option === questions[questionNumber].answer ? 'btn-success' : 'btn-danger'}`}
-                type="button"
-                disabled={!!answer}
-                onClick={() => dispatch({ type: 'giveAnswer', payload: option })}
-            >
-                {option}
-            </button>
-        )}
-      </div>
-      {answer ?
-        answer === questions[questionNumber].answer ?
-            <p>Correct!</p> :
-            <p>Unfortunate.  Your answer: {answer}.  Correct answer: {questions[questionNumber].answer}</p>
-      :
-      <></>}
-      <div className="Navigator">
-            <button
-                disabled={!answer || questionNumber === 0}
-                className="btn btn-secondary" 
-                type="button"
-                onClick={() => dispatch({ type: 'previousQuestion'})}
-            >
-                Previous
-            </button>
-            <button
-                disabled={!answer || questionNumber === (questions.length-1)}
-                className="btn btn-primary" 
-                type="button"
-                onClick={() => dispatch({ type: 'nextQuestion'})}
-            >
-                Next
-            </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="Prepositions">
+            <p>Choose the correct preposition for the verb</p>
+            <p className="h3 Verb">{questions[questionNumber].word}</p>
+            <div className="Choices">
+                {options.map(option =>
+                    <button
+                        className={`btn Choices__Button ${!answer && 'btn-light'} ${answer === option ? 'btn-lg' : 'btn-sm'} ${answer && option === questions[questionNumber].answer ? 'btn-success' : 'btn-danger'}`}
+                        type="button"
+                        disabled={!!answer}
+                        onClick={() => dispatch({ type: 'giveAnswer', payload: option })}
+                    >
+                        {option}
+                    </button>
+                )}
+            </div>
+            {answer ?
+                answer === questions[questionNumber].answer ?
+                    <p>Correct!</p> :
+                    <p>Unfortunate.  Your answer: {answer}.  Correct answer: {questions[questionNumber].answer}</p>
+                :
+                <></>
+            }
+            {
+                answer ?
+                    questions[questionNumber].otherAnswers.length > 0 ?
+                        <p>Other correct answers: {questions[questionNumber].otherAnswers.join(', ')}</p> :
+                        <></> :
+                    <></>
+            }
+            <div className="Navigator">
+                <button
+                    disabled={!answer || questionNumber === 0}
+                    className="btn btn-secondary"
+                    type="button"
+                    onClick={() => dispatch({ type: 'previousQuestion' })}
+                >
+                    Previous
+                </button>
+                <button
+                    disabled={!answer || questionNumber === (questions.length - 1)}
+                    className="btn btn-primary"
+                    type="button"
+                    onClick={() => dispatch({ type: 'nextQuestion' })}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Prepositions
