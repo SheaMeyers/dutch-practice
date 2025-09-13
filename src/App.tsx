b05b23cf14a8374736fa0fcb437156be4fc214e4
SheaMeyers <shea.meyers@gmail.com>
2025-09-13 14:00:44 +0200

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import './index.css';
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
 import Prepositions from './prepositions/Prepositions';
 import DeHet from './deHet/DeHet';
 
-const App = () => {
-  return (
-    <Router>
-      <div className='main'>
+const Sidebar = () => (
+  <nav className='flex-grow-2 sidebar'>
+    <ul className="nav flex-column">
+      <li className="nav-item mb-2">
+        <Link className="nav-link" to="/">Prepositions</Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" to="/de-het">De/Het</Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <div className='main'>
+      <Sidebar />
+      <div className='flex-grow-1'>
         <Routes>
           <Route path="/" element={<Prepositions />} />
           <Route path="/de-het" element={<DeHet />} />
         </Routes>
       </div>
-    </Router>
-  );
-}
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
